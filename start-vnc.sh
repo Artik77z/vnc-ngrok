31190266e1256b97fde1f150a844be8867d3a72f
Artik77z <artik77z.stock@outlook.com>
2022-04-22 14:33:11 +0000

    Update start-vnc.sh

--- start-vnc.sh
+++ start-vnc.sh
@@ -1,3 +1,6 @@
 sudo vncserver
 sudo xfce4-panel -r && xfwm4 --replace
 ./ngrok tcp 127.0.0.1:5901
+sleep 3
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb
